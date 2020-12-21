648d1958db62c90e2a61b3a13963d8fadc46237d
SunHwan-Park <meshim435@gmail.com>
2020-12-22 05:11:28 +0900

    Update projects

---
+++ src/routes/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./About.css";
+
+function About(props) {
+  return (
+    <div className="about__container">
+      <span>
+        About this page: I build it because I love movies.
+      </span>
+    </div>
+  );
+}
+
+export default About;
